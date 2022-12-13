0b94ac68e28d6dfe0f2c68c8339b6c23d1de1b40
fmfrom422yd <103907781+fmfrom422yd@users.noreply.github.com>
2022-12-13 05:24:43 +0000

    ああ

--- pages/recipe.vue
+++ pages/recipe.vue
@@ -1,39 +1,57 @@
 <template>
-  <div class="container text-center my-5">
+<div class="container text-center my-5">
     <h1>{{ data.title }}</h1>
     <div class="alert alert-primary" role="alert">{{ data.description }}</div>
-  </div>
-  <div class="container mb-5">
-    <div class="card mb-3 mx-auto">
-      <div class="row g-0">
+</div>
+<div class="container mb-5">
+<div class="card mb-3 mx-auto">
+<div class="row g-0">
         <div class="col-md-4">
-          <img
+            <img
             src="https://img.cpcdn.com/recipes/3695776/894x1461s/03855e67f64548a05b1f995faabe8f57?u=12868943&p=1455726325"
             class="img-fluid rounded-start" alt="...">
         </div>
         <div class="col-md-8">
-          <div class="card-body">
+        <div class="card-body">
             <h5 class="card-title">{{material}}</h5>
             <div class="card-text">
-              <ul>
+            <ul>
                 <li v-for="item in data.material">{{ item.text }}</li>
-              </ul>
+            </ul>
             </div>
-          </div>
+</div>
         </div>
-      </div>
+</div>
     </div>
-  </div>
-  <div class="container mb-5">
-    <h2><span class="card-title">{{howtomake}}</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+</div>
+<div class="container mb-5">
+    <h2 span class="card-title">{{howtomake}}</h2>
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
         <div class="col border p-2">
         <div class="mb-2"><span class="badge bg-secondary">{{ri.id}}</span></div>
         <p>{{rp.text}}</p>
         </div>
         </div>
+
+        <div class="container mb-5">
+<div class="row gx-5 ">
+        <div class="col-sm">
+    <h3>{{trick}}</h3>
+
+    <p  v-for="kotu in point">{{kotu.text}}</p>
+
+        </div>
+
+
+<div class="col-sm">
+<h3>{{thoughts}}</h3>
+
+    <p v-for="bg in background">{{bg.text}}</p>
+
+        </div>
+</div>
     </div>
-    </div>
+</div>
 </template>
 <script setup>
     const data = useRecipe()
