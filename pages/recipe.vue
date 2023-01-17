d0c457ebdbf5413092d4d43026708ce7285b4c58
fmfrom422yd <103907781+fmfrom422yd@users.noreply.github.com>
2023-01-17 05:41:53 +0000

    福島

--- pages/recipe.vue
+++ pages/recipe.vue
@@ -1,58 +1,55 @@
 <template>
-<div class="container text-center my-5">
+  <div class="container text-center my-5">
     <h1>{{ data.title }}</h1>
     <div class="alert alert-primary" role="alert">{{ data.description }}</div>
-</div>
-<div class="container mb-5">
-<div class="card mb-3 mx-auto">
-<div class="row g-0">
+  </div>
+  <div class="container mb-5">
+    <div class="card mb-3 mx-auto">
+      <div class="row g-0">
         <div class="col-md-4">
-            <img
-            src="https://img.cpcdn.com/recipes/3695776/894x1461s/03855e67f64548a05b1f995faabe8f57?u=12868943&p=1455726325"
-            class="img-fluid rounded-start" alt="...">
+          <img
+            src="@/curry.jpg" alt="...">
         </div>
         <div class="col-md-8">
-        <div class="card-body">
-            <h5 class="card-title">{{material}}</h5>
+          <div class="card-body">
+            <h2 class="card-title">{{data.headline}}</h2>
             <div class="card-text">
-            <ul>
+              <ul>
                 <li v-for="item in data.material">{{ item.text }}</li>
-            </ul>
+              </ul>
             </div>
-</div>
+          </div>
         </div>
-</div>
+      </div>
     </div>
-</div>
-<div class="container mb-5">
-    <h2 span class="card-title">{{howtomake}}</h2>
-        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
-        <div class="col border p-2">
-        <div class="mb-2"><span class="badge bg-secondary">{{ri.id}}</span></div>
-        <p>{{rp.text}}</p>
+  </div>
+  <div class="container mb-5">
+    <h2>{{data.headline2}}</h2>
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+      <div class="col boder p-2" v-for="(todoL,index) in data.todoList.items" :key="todoL.id" :text="todoL.text">
+        <div class="msg-wrapper">
+          <div class="mb-2">{{todoL.id}}
+          </div>
+      <p>{{todoL.text}}</p>
         </div>
-        </div>
-
-        <div class="container mb-5">
-<div class="row gx-5 ">
-        <div class="col-sm">
-    <h3>{{trick}}</h3>
-
-    <p  v-for="kotu in point">{{kotu.text}}</p>
-
-        </div>
-
-
-<div class="col-sm">
-<h3>{{thoughts}}</h3>
-
-    <p v-for="bg in background">{{bg.text}}</p>
-
-        </div>
-</div>
+      </div>
     </div>
-</div>
+  </div>
+  <div class="container mb-5">
+    <div class="row gx-5">
+      <div class="col-sm">
+        <h3>{{data.headline3}}</h3>
+        <p v-for="point in data.points">{{point.text}}</p>
+      </div>
+      <div class="col-sm">
+        <h3>{{data.headline4}}</h3>
+      <p v-for="origin in data.origins">
+      {{origin.text}}</p>
+        
+      </div>
+    </div>
+  </div>
 </template>
 <script setup>
-    const data = useRecipe()
+  const data = useRecipe()
 </script>
